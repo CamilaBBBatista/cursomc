e2080ad3651a9f2b81f6ac25b5906671088a7d0b
Camila Batista <milabbb-2a@hotmail.com>
2020-12-09 21:34:35 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/camilabatista/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.camilabatista.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.camilabatista.cursomc.domain.Pedido;
+import com.camilabatista.cursomc.repositories.PedidoRepository;
+import com.camilabatista.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);					
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id +
+				" Tipo: " + Pedido.class.getName()));
+	}
+
+}
