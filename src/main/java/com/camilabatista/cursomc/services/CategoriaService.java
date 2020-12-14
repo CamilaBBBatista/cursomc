4da00160ba3a0590668c3410deab405585305b80
Camila Batista <milabbb-2a@hotmail.com>
2020-12-14 11:51:20 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/camilabatista/cursomc/services/CategoriaService.java
+++ src/main/java/com/camilabatista/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.camilabatista.cursomc.domain.Categoria;
+import com.camilabatista.cursomc.dto.CategoriaDTO;
 import com.camilabatista.cursomc.repositories.CategoriaRepository;
 import com.camilabatista.cursomc.services.exceptions.DataIntegrityException;
 import com.camilabatista.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDto(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
